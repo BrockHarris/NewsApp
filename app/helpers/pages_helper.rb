1435352a8b13065740f4f555a7df3b794dec46d7
Brock <btharris781@gmail.com>
2013-04-07 03:06:05 -0400

    design stuff

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,7 +3,6 @@ module PagesHelper
       if datatype == "highvalue"
         json = JSON.parse(open("#{@source_url}/v3/highvalue?access_token=#{@access_token}&limit=#{@limit}").read)
         @result = json['data']['values']
-       # @result = ActiveSupport::JSON.decode(result)
       elsif datatype == "toplinks"
         json = JSON.parse(open(@top_links).read)
         @result = json['data']['phrases']
