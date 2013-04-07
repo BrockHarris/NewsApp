869e6b6766391d42fb50922662733184ca335ced
SS <git@push.com>
2013-04-07 00:50:11 -0400

    hot phrases

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,8 @@ class PagesController < ApplicationController
 		@source_url = "https://api-ssl.bitly.com"
 		@access_token = "4d8175b24369da3c224352ebf1687e513cc714b9"
 		@limit = 10
-		@return_top_links = "#{@source_url}/v3/realtime/bursting_phrases?access_token=#{@access_token}&limit=10"
-		@search_url = "#{@source_url}/v3/search?access_token=#{@access_token}&query=#{@query}&limit=10&fields=aggregate_link%2Ctitle%2Curl"
+		@top_links = "#{@source_url}/v3/realtime/hot_phrases?access_token=#{@access_token}&limit=#{@limit}"
+		@search_url = "#{@source_url}/v3/search?access_token=#{@access_token}&query=#{@query}&limit=#{@limit}&fields=aggregate_link%2Ctitle%2Curl"
 		@query = "sample"
 	end
 end
