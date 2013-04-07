7645535a2c15a8aed7c9c67bbee8d6b83610293a
Brock <btharris781@gmail.com>
2013-04-06 21:12:13 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsApp::Application.config.secret_token = '00c25326247ddb7ab45165014fce87c54faf9de0760bbb6d3d2840ca2288d3f89a88cfd222d7893a60a7db219964ca448201cd5542648431561e971a01197fb5'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+NewsApp::Application.routes.draw do
+  
+  root :to => 'pages#home'
+
+end
